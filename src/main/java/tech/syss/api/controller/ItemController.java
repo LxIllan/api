5bf2bd12c6cf9ffd211b1bc12aef2b2d3d04e3d7
LFIllan <lf.illan@mavi.mx>
2025-03-13 12:55:11 -0600

    Add logging to ItemController.

--- src/main/java/tech/syss/api/controller/ItemController.java
+++ src/main/java/tech/syss/api/controller/ItemController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -21,6 +23,7 @@ import tech.syss.api.specification.ItemSpecification;
 @RequestMapping("/api/items")
 public class ItemController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ItemController.class);
     private ItemService itemService;
 
     public ItemController(ItemService itemService) {
@@ -41,6 +44,7 @@ public class ItemController {
     public ResponseEntity<Object> get(@PathVariable Long id) {
         Object item = itemService.get(id);
         if (item == null) {
+            logger.info("Item with id {} not found.", id);
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok().body(item);
