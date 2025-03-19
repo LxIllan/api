de368290d642e22041693aaa9737045386fd5429
LFIllan <lf.illan@mavi.mx>
2025-03-19 16:53:55 -0600

    Remove unnecessary @Repository annotation from RoleRepository.

--- src/main/java/tech/syss/api/repository/RoleRepository.java
+++ src/main/java/tech/syss/api/repository/RoleRepository.java
@@ -1,10 +1,8 @@
 package tech.syss.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import tech.syss.api.model.Role;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 }
