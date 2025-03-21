b86291cd65ccf745871e791db78612d808a01967
LFIllan <lf.illan@mavi.mx>
2025-03-21 10:11:24 -0600

    Add unit tests for MemberRepository to validate findByCode functionality.

---
+++ src/test/java/tech/syss/api/repository/MemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package tech.syss.api.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import tech.syss.api.model.Member;
+import tech.syss.api.model.Membership;
+
+@DataJpaTest
+public class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MembershipRepository membershipRepository;
+
+    @Test
+    void testFindByCode() {
+        Membership membership = new Membership("Monthly", 1, 0, 350.0);
+        membershipRepository.save(membership);
+        Member member = new Member(1L, "0405", "Fernando", "Illan", "Fernando.Illan@syss.tech", "3861063066", "", membership, LocalDateTime.now().plusMonths(1));
+        memberRepository.save(member);
+
+        Optional<Member> result = memberRepository.findByCode("0405");
+        assertTrue(result != null);
+        assertEquals("Fernando", result.get().getName());
+    }
+}
