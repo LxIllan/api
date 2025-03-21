9d43070a09359e7520a7585c6a99d3d7d697f087
LFIllan <lf.illan@mavi.mx>
2025-03-21 10:11:14 -0600

    Refactor: Simplify ApiApplicationTests by removing unnecessary contextLoads test method.

--- src/test/java/tech/syss/api/ApiApplicationTests.java
+++ src/test/java/tech/syss/api/ApiApplicationTests.java
@@ -1,13 +1,6 @@
 package tech.syss.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+class ApiApplicationTests {}
